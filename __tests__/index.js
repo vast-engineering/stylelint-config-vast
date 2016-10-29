3cafc461dc62c80cd60c8b0f2cd83d53e126b349
Vladimir Siljkovic <vladimirs@vast.com>
2016-10-29 23:04:13 +0200

    Lint code

--- __tests__/index.js
+++ __tests__/index.js
@@ -14,29 +14,29 @@ const invalidCss = (
 }
 `);
 
-test('Should flag no warnings with valid CSS.', t => {
-    return stylelint.lint({
+test('Should flag no warnings with valid CSS.', t =>
+    stylelint.lint({
         code: validCss,
-        config: config
+        config
     })
-    .then(data => {
+    .then((data) => {
         const { errored, results } = data;
         const { warnings } = results[0];
         t.falsy(errored, 'Not errored.');
         t.is(warnings.length, 0, 'Flags no warnings.');
-    });
-});
-
-test('Should flag a warning with invalid CSS.', t => {
-    return stylelint.lint({
-        code: invalidCss,
-        config: config
     })
-    .then(data => {
+);
+
+test('Should flag a warning with invalid CSS.', t =>
+    stylelint.lint({
+        code: invalidCss,
+        config
+    })
+    .then((data) => {
         const { errored, results } = data;
         const { warnings } = results[0];
         t.truthy(errored, 'Errored.');
         t.is(warnings.length, 1, 'Flags one warning.');
         t.is(warnings[0].text, 'Expected a leading zero (number-leading-zero)', 'correct warning text');
-    });
-});
+    })
+);
